6b3102e9936ac11047de4622e13cff77a886bff4
eljesa-kqiku <eljesakqiku@gmail.com>
2024-08-15 13:27:44 +0200

    Added Regex Validation Rule

---
+++ src/rules/RegexValidationRule.ts
@@ -0,0 +1,31 @@
+import IValidationRule from './IValidationRule.ts'
+
+export default class RegexValidationRule implements IValidationRule {
+    private errorMessage: string = 'Field is invalid.'
+    private regexString
+
+    constructor(regex: String) {
+        this.regexString = regex
+    }
+
+    isValid(param: string): [boolean, string] {
+        const isValid = new RegExp(this.regexString).test(param)
+        return [isValid, isValid ? '' : this.errorMessage]
+    }
+
+    isMatch(type: string): boolean {
+        return type.toLowerCase() === 'regex'
+    }
+
+    // @ts-ignore
+    setParams(params: any): void {
+    }
+
+    setErrorMessage(message: string): void {
+        this.errorMessage = message
+    }
+
+    setRegex(regex: String) {
+        this.regexString = regex
+    }
+}
